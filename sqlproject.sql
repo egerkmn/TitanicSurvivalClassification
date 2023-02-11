41680f89fd8c2394640197cccff25094e6121d48
Ege <123592010+egerkmn@users.noreply.github.com>
2023-02-11 11:47:03 +0300

    Update sqlproject.sql

--- sqlproject.sql
+++ sqlproject.sql
@@ -1,9 +1,7 @@
--- TABLE
-CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
- 
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
+--Query Desc: GROUP BY with multi-columns and extracting date information from datetime column
+--Data Desc: an app installation table containing event_time, user_id, platform(IOS/ANDROID), Network(download channel), and country
+--Functions: EXTRACT(), GROUP BY
+SELECT EXTRACT(DATE FROM event_time) AS event_date, platform, network, country,
+COUNT(*) AS install, COUNT(DISTINCT user_id) AS user
+FROM table_app_install
+GROUP BY EXTRACT(DATE FROM event_time), platform, network, country
