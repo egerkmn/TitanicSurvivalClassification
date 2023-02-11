6a65634d0fd2d9b6336322c8eb498ee5b83f7cba
Ege <123592010+egerkmn@users.noreply.github.com>
2023-02-11 11:47:13 +0300

    Delete sqlproject.sql

--- sqlproject.sql
+++
@@ -1,7 +0,0 @@
---Query Desc: GROUP BY with multi-columns and extracting date information from datetime column
---Data Desc: an app installation table containing event_time, user_id, platform(IOS/ANDROID), Network(download channel), and country
---Functions: EXTRACT(), GROUP BY
-SELECT EXTRACT(DATE FROM event_time) AS event_date, platform, network, country,
-COUNT(*) AS install, COUNT(DISTINCT user_id) AS user
-FROM table_app_install
-GROUP BY EXTRACT(DATE FROM event_time), platform, network, country
